85426b3a2584703e0c0f2efbea6edba36c4c9c53
Zach Wellens <zachwellens990@gmail.com>
2023-07-12 16:21:16 -0400

    Created Database and Tables

---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataType.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataType.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        date_created: {
+            type: DataTypes.DATE
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+)
+
+module.exports = Comment;
